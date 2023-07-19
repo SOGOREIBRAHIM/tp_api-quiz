a08846adf83e0c7fb840a01fd6d03b1fb750bb78
sogore <sogoreibrahim135@gmail.com>
2023-07-19 20:25:53 +0000

    squellet

---
+++ src/main/java/com/api_quiz/api/ApiQuizApplication.java
@@ -0,0 +1,13 @@
+package com.api_quiz.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiQuizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api_quiz/api/ApiQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api_quiz.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
