cdd2fd696c01006b6972012aad4d445a0de017ab
drissatogo <togo.drissa@odc.local>
2023-07-22 15:15:50 +0000

    test

--- src/main/java/com/api_quiz/api/services/IQuestionService.java
+++ src/main/java/com/api_quiz/api/services/IQuestionService.java
@@ -1,2 +1,41 @@
-package com.api_quiz.api.services;public interface IQuestionService {
+package com.api_quiz.api.services;
+import com.api_quiz.api.entites.Question;
+import com.api_quiz.api.repository.IQuestionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public interface IQuestionService {
+    public class QuestionService {
+
+        private final IQuestionRepository questionRepository;
+
+        @Autowired
+        public QuestionService(IQuestionRepository questionRepository) {
+            this.questionRepository = questionRepository;
+        }
+
+        public List<Question> getAllQuestions() {
+            return questionRepository.findAll();
+        } // Récupérer toutes les questions depuis la base de données
+
+        public Question getQuestionById(Long id) {
+            return questionRepository.findById(id).orElse(null);
+        } // Récupérer une question par son identifiant
+
+        public Question saveQuestion(Question question) {
+            return questionRepository.save(question);
+        }  // Sauvegarder une nouvelle question dans la base de données
+
+        public void deleteQuestionById(Long id) {
+            questionRepository.deleteById(id);
+        }  // Supprimer une question par son identifiant
+
+    }
 }
+
+
+
--- src/main/java/com/api_quiz/api/services/IReponseService.java
+++ src/main/java/com/api_quiz/api/services/IReponseService.java
@@ -1,2 +1,44 @@
-package com.api_quiz.api.services;public interface IReponseService {
+package com.api_quiz.api.services;
+
+import com.api_quiz.api.entites.Reponse;
+import com.api_quiz.api.repository.IReponseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+
+public interface IReponseService {
+    @Service
+    public class ReponseService {
+
+        private final IReponseRepository reponseRepository;
+
+        @Autowired
+        public ReponseService(IReponseRepository reponseRepository) {
+            this.reponseRepository = reponseRepository;
+        }
+
+        // Récupérer toutes les réponses depuis la base de données
+        public List<Reponse> getAllReponses() {
+            return reponseRepository.findAll();
+        }
+
+        // Récupérer une réponse par son identifiant
+        public Reponse getReponseById(Long id) {
+            return reponseRepository.findById(id).orElse(null);
+        }
+
+        // Sauvegarder une nouvelle réponse dans la base de données
+        public Reponse saveReponse(Reponse reponse) {
+            return reponseRepository.save(reponse);
+        }
+
+        // Supprimer une réponse par son identifiant
+        public void deleteReponseById(Long id) {
+            reponseRepository.deleteById(id);
+        }
+    }
+
+
 }
